79e24e32e2ee71ec359d4ee18f891a8b89d22749
njee1030 <nitin.joshi1030@gmail.com>
2021-12-28 21:52:33 +0530

    UI completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123123*</div>
+        <div className="current-operand">1223</div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
